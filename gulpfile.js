8a608835e59ea468f2c8524c82a7d1d33c09a364
OlenaPustova <i.am.elena.p@gmail.com>
2020-11-13 21:48:44 +0200

    hero block

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./sass/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./markup/css'));
 });
